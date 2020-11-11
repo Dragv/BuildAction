985f827a7763e5a24b59929ae049f83d549977b9
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2020-11-11 13:20:08 -0600

    Added secrets modify

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { octokit } = require("@octokit/core");
 const axios = require('axios');
 
 const main = async() => {
   try {
-    const { name } = await octokit.request('GET /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-      owner: 'Dragv',
-      repo: 'BuildAction',
-      secret_name: 'BUILD_URL'
-    });
-    console.log(name);
-    await axios.default.get(name);
+    const BUILD_URL = core.getInput('BUILD_URL');
+
+    axios.default.get(BUILD_URL);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
 main();
