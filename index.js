e3b56d4d7c77a034173924c449306331fdffb2a6
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2020-11-11 13:01:55 -0600

    Added secrets modify

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { octokit } = require("@octokit/core");
 const axios = require('axios');
 
 const main = async() => {
